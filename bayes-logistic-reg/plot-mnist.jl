f596bb9aef609f751ea4f2bfb576f0670335b6a5
ywwes26 <49973874+ywwes26@users.noreply.github.com>
2024-06-25 11:03:14 +0800

    Update plot-mnist.jl

--- bayes-logistic-reg/plot-mnist.jl
+++ bayes-logistic-reg/plot-mnist.jl
@@ -1,6 +1,4 @@
 ### for 3 and 5 ###
-
-
 using Plots
 using JLD2, FileIO
 using LaTeXStrings
@@ -22,7 +20,6 @@ plot!(p1, acc_time, acc, label="", linewidth=2)
 plot!(p1, acc_time, acc, label="", linewidth=2, color = :orange)
 
 
-
 # step size = 1e-3
 @load "35_MH_stepsize1e-3_step10000.jld2" acc acc_time
 p2 = plot(acc_time, acc, xlims=(0, 2.5), ylims=(0.4, 1), xticks = range(0, 4, 9), yticks = range(0.3, 1, 8), title="step size=0.001", label="", linewidth=2)
@@ -34,7 +31,6 @@ plot!(p2, acc_time, acc, label="", linewidth=2)
 plot!(p2, acc_time, acc, label="", linewidth=2)
 @load "35_HMC_lf5_clip_stepsize1e-3_step10000.jld2" acc acc_time
 plot!(p2, acc_time, acc, label="", linewidth=2, color = :orange)
-
 
 
 # step size = 2e-3
@@ -50,7 +46,6 @@ plot!(p3, acc_time, acc, label="", linewidth=2)
 plot!(p3, acc_time, acc, label="", linewidth=2, color = :orange)
 
 
-
 # step size = 4e-3
 @load "35_MH_stepsize4e-3_step10000.jld2" acc acc_time
 p4 = plot(acc_time, acc, label="MH", xlims=(0, 2.5), ylims=(0.4, 1), xticks = range(0, 4, 9), yticks = range(0.3, 1, 8), title="step size=0.004", linewidth=2)
@@ -64,4 +59,4 @@ plot!(p4, acc_time, acc, label="MALA", linewidth=2)
 plot!(p4, acc_time, acc, label="HMC-5", linewidth=2, color = :orange)
 
 
-p_time = plot(p1, p2, p3, p4, layout=l, xlabel="Time(s)", plot_title="7 and 9: Test Accuracy vs Time", size=(800, 600), dpi=1000, legend=:bottomright)
+p_time = plot(p1, p2, p3, p4, layout=l, xlabel="Time(s)", plot_title="3 and 5: Test Accuracy vs Time", size=(800, 600), dpi=1000, legend=:bottomright)
